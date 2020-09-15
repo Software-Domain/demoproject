af86e3b92f1208113ac8bbf51fc61ed5dd35e28e
JYOTIRANJAN  NAYAK <nayakjyotiranjan023@gmail.com>
2020-09-15 12:19:24 +0530

    New User1 class in bean

---
+++ demobootmvc/src/main/java/com/ray/bootweb/bean/User1.java
@@ -0,0 +1,23 @@
+package com.ray.bootweb.bean;
+
+public class User1 {
+	public int getRegno() {
+		return regno;
+	}
+	public void setRegno(int regno) {
+		this.regno = regno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private int regno;
+	private String name;
+	public User1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
